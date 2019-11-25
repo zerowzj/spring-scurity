0ac636b53cc5226234402fe14c7b1116fc9db491
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-25 16:51:01 +0800

    更新代码

--- src/main/java/study/springsecurity/auth/config/WebSecurityCfg.java
+++ src/main/java/study/springsecurity/auth/config/WebSecurityCfg.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
@@ -15,25 +16,29 @@ public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //
         http.formLogin() //定义当需要用户登录时候，转到的登录页面
-                .loginPage("login.html")     //设置登录跳转页面controller，也可以直接跳转页面
-                .loginProcessingUrl("/login") //自定义登录页面的表单提交地址
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .and()
-                .authorizeRequests()
+                .loginPage("login.html")       //设置登录跳转页面controller，也可以直接跳转页面
+                .loginProcessingUrl("/login"); //自定义登录页面的表单提交地址
+        //
+        http.logout()
+                .logoutUrl("/logout");
+        //
+        http.authorizeRequests()
                 .antMatchers("/", "/login.html")
-                .permitAll() //
-                .anyRequest()
-                .authenticated()                //默认其它的请求都需要认证，这里一定要添加
-                .and()
-                .csrf().disable();
+                .permitAll()
+                .anyRequest()  //默认其它的请求都需要认证，这里一定要添加
+                .authenticated();
+        //
+        http.csrf().disable();
     }
-
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
     }
 }
