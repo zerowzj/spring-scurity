5c744b3e2c0404eb99d20de47b4703b55dd25d57
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-25 16:15:30 +0800

    更新代码

--- src/main/java/study/springsecurity/auth/config/WebSecurityCfg.java
+++ src/main/java/study/springsecurity/auth/config/WebSecurityCfg.java
@@ -2,59 +2,25 @@ package study.springsecurity.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@Order(1)
 public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.formLogin() //表单登录
-//                 .loginPage("/login.html") //设置登录跳转页面controller、也可以直接跳转页面
-//                .loginProcessingUrl("/login") //自定义登录页面的表单提交地址
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/login")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .csrf()
-//                .disable();
-
-//        http.formLogin()
-//                .and()
-//                .authorizeRequests().antMatchers("/oauth/**").permitAll()
-//                .anyRequest()
-//                .authenticated();
-
-        http.requestMatchers()
-                .antMatchers("/login", "/oauth/authorize")
+        http.formLogin() //表单登录
+                .loginPage("/login.html")     //设置登录跳转页面controller、也可以直接跳转页面
+                .loginProcessingUrl("/login") //自定义登录页面的表单提交地址
                 .and()
                 .authorizeRequests()
-                .anyRequest()
-                .authenticated()
+                .antMatchers("/login")
+                .permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .permitAll();
-
-//        http.authorizeRequests()
-//                .antMatchers("/oauth/**", "/login/**", "/logout")
-//                .permitAll()
-//                .anyRequest().authenticated()   // 其他地址的访问均需验证权限
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .and()
-//                .logout().logoutSuccessUrl("/");
+                .csrf().disable();
     }
 
     /**
@@ -66,33 +32,4 @@ public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
         AuthenticationManager manager = super.authenticationManagerBean();
         return manager;
     }
-
-    /**
-     * 用户信息服务
-     */
-//    @Bean
-//    @Override
-//    protected UserDetailsService userDetailsService() {
-//        UserDetails userDetails = User.withUsername("wzj")
-//                .password("{noop}123")
-//                .roles("USER")
-//                .build();
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(userDetails);
-//        return manager;
-//    }
-
-
-    /**
-     * 定义用户名 密码和角色
-     * 1.
-     * 2.
-     */
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("wang")
-//                .password("{noop}123")
-//                .authorities("USER");
-//    }
 }
