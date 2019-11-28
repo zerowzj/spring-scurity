ef2e8ac6697b85dbd1c8e1a17721c7edd6a9ca3c
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-28 18:32:19 +0800

    更新代码

--- src/main/java/study/springboot/security/auth/config/WebSecurityCfg.java
+++ src/main/java/study/springboot/security/auth/config/WebSecurityCfg.java
@@ -1,6 +1,7 @@
 package study.springboot.security.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,45 +37,48 @@ public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //（☆）过滤器
-//        http.addFilterBefore(watchDogFilter, UsernamePasswordAuthenticationFilter.class)
-//        ;
-        //（☆）认证
-//        http.httpBasic(); //Basic登录方式
-        http.formLogin() //表单登录，需要登录时，转到的登录页面
+        //（▲）过滤器
+        http.addFilterBefore(watchDogFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
+        //（▲）认证
+        //Basic登录
+        http.httpBasic();
+        //表单登录
+        http.formLogin() //需要登录时，转到的登录页面
                 .loginPage("/login.html") //登录跳转页面controller或页面
                 .loginProcessingUrl("/login") //登录表单提交地址
-                .defaultSuccessUrl("/main.html", true) //
-                //.successForwardUrl("/") //
+                .defaultSuccessUrl("/main.html", true) //默认登录成功url
+                //.successForwardUrl("/") //登录成功跳转url
                 //.successHandler(loginSuccessHandler) //登录成功处理器
                 .failureUrl("/login.html?login_failure") //登录失败url，前端可通过url中是否有error来提供友好的用户登入提示
-                //.failureForwardUrl()
-                .failureHandler(loginFailureHandler) //登录失败处理器
+                //.failureForwardUrl() //登录失败跳转url
+                //.failureHandler(loginFailureHandler) //登录失败处理器
                 .usernameParameter("username")
                 .passwordParameter("password")
                 .permitAll()
-                .isCustomLoginPage()
         ;
-        //（☆）授权
+        //（▲）授权
         http.authorizeRequests() //请求授权
-//                .accessDecisionManager() //
-//                .withObjectPostProcessor() //
-//                .antMatchers("/login**")
-//                .permitAll() //不需要权限认证
+                //.accessDecisionManager() //
+                //.withObjectPostProcessor() //
+                //.antMatchers("/login**")
+                //.permitAll() //不需要权限认证
                 .anyRequest()  //任何请求
                 .authenticated() //需要身份认证
         ;
-        //（☆）
+        //（▲）
         http.exceptionHandling()
-                .accessDeniedHandler(accessDeniedHandler)
-                .accessDeniedPage("/403")
+                .accessDeniedPage("/403") //无权页面
+        //.accessDeniedHandler(accessDeniedHandler) //无权处理器
+        //.authenticationEntryPoint() //认证入口
         ;
-        //（☆）注销
+        //（▲）注销
         http.logout()
-                .logoutUrl("/logout")  //
-                .logoutSuccessUrl("/login.html") //
-                .logoutSuccessHandler(logoutSuccessHandler)
+                .logoutUrl("/logout")  //注销url
+                .logoutSuccessUrl("/login.html") //注销成功url
+        //.logoutSuccessHandler(logoutSuccessHandler) //注销成功处理器
         ;
+        SecurityProperties securityProperties ;
         //（☆）会话
         http.sessionManagement()
                 .invalidSessionUrl("/login.html?session_invalid")
@@ -82,7 +86,7 @@ public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
 //                .maxSessionsPreventsLogin(false) // 当达到最大值时，是否保留已经登录的用户
 //                .expiredSessionStrategy(new CustomExpiredSessionStrategy()) // 当达到最大值时，旧用户被踢出后的操作
         ;
-        //（☆）其他
+        //（▲）其他
         http.csrf().disable() //关闭跨站请求防护
         ;
 
