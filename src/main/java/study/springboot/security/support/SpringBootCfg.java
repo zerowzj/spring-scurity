49fc87747a00badc74d128f086382fa4956eeedd
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-03 11:52:09 +0800

    更新代码

--- src/main/java/study/springboot/security/support/SpringBootCfg.java
+++ src/main/java/study/springboot/security/support/SpringBootCfg.java
@@ -1,9 +1,9 @@
 package study.springboot.security.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @SpringBootApplication(scanBasePackages = "study.springboot.security")
 public class SpringBootCfg {
 }
